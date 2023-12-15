fc47a27fb2903f5489d7fe8422874e454a8ffdee
Tonylowkey <antvargas7542@gmail.com>
2023-12-15 18:49:09 -0500

    Starter Pack
    Camera pathfinding and movement

---
+++ TinyArmies/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Seeker seeker;
+    private Rigidbody2D rb;
+    private Path path;
+    private int currentWaypoint = 0;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CalculatePathToMouse();
+        }
+
+        // Check if the path calculation is done
+        if (path == null)
+            return;
+
+        if (currentWaypoint < path.vectorPath.Count)
+        {
+            // Move towards the current waypoint
+            Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - (Vector2)rb.position).normalized;
+            rb.velocity = direction * moveSpeed;
+
+            // Check if the player is close enough to the current waypoint to move to the next one
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+            if (distance < 0.1f)
+            {
+                currentWaypoint++;
+
+                if (currentWaypoint == path.vectorPath.Count)
+                {
+                    rb.velocity = Vector2.zero;
+                    path = null;
+                }
+            }
+        }
+    }
+
+    void CalculatePathToMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        seeker.StartPath(rb.position, mousePos, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        // A* path calculation is complete, update the path
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+}
