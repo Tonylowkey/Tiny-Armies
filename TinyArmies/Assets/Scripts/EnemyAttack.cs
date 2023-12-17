f2337be0a8ae663ad6aea3c1eb56871cde4d2ef8
Tonylowkey <antvargas7542@gmail.com>
2023-12-17 17:16:47 -0500

    Attack

---
+++ TinyArmies/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public int damagePerTick = 5; 
+    public float tickRate = 1f; 
+    public float damageRadius = 3f; 
+    private float timeSinceLastTick;
+
+    private void Update()
+    {
+        if (Time.time - timeSinceLastTick >= tickRate)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, damageRadius);
+
+            foreach (Collider2D collider in colliders)
+            {
+                if(collider.tag == "playerMilitia"){
+                    GroupHealth health = collider.GetComponent<GroupHealth>();
+                    if (health != null)
+                    {
+                        health.TakeDamage(damagePerTick);
+                    }
+                }
+            }
+
+            timeSinceLastTick = Time.time;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw a wire sphere to visualize the damage radius in the scene view
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, damageRadius);
+    }
+}
